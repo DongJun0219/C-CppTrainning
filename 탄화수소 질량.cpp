d4acb12a8dbd82ce1759ba29301b209dd10e72c7
seola <seolalny@gmail.com>
2022-02-27 14:50:00 +0900

    c++

--- 탄화수소 질량.cpp
+++ 탄화수소 질량.cpp
@@ -6,8 +6,9 @@ int main(){
 	scanf("%s", &a);
 	if(a[1]=='H') b=12;
 	else b=12*(a[1]-48);
-	if(a[2]!=0 && a[2]!='H') c=(a[2]-48);
-	else if(a[3]!=0 && a[3]!='H') c=(a[3]-48);
+	if(a[2]==0) c=1;
+	else if(a[2]!='H') c=(a[2]-48);
+	else if(a[3]!=0) c=(a[3]-48);
 	else c=1;
 	sum=b+c;
 	printf("%d", sum);
